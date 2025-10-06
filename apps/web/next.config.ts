f462131cd84a37856fab86483b84a17a624d5dd0
spagestic <steelmax666@gmail.com>
2025-10-06 09:39:24 +0800

    using downloaded company logos

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	typedRoutes: true,
+  typedRoutes: true,
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
 };
 
 export default nextConfig;
